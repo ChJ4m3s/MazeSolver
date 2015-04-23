41764e2c05303a7d1681ed4b0fb3301ec3507636
Giacomo Chiarot <Giacomo@MacBook-Pro-di-Giacomo.local>
2015-04-23 17:53:51 +0200

    Initial Commit

---
+++ Maze_solver/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Maze_solver
+//
+//  Created by Giacomo Chiarot on 23/04/15.
+//  Copyright (c) 2015 Giacomo Chiarot. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
